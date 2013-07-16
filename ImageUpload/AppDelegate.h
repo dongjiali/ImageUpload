9422a67f369d0b7e1a7b4d3f000194cd42f5991e
Curry <meoliver@localhost.(none)>
2013-07-16 09:55:32 +0800

    Initial Commit

---
+++ ImageUpload/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageUpload
+//
+//  Created by Curry on 13-7-16.
+//  Copyright (c) 2013年 curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
